49cdb18001754370d8d4ac958b71ba7eb2ee8e09
AyushAgrawal03 <ayush03.india@gmail.com>
2026-08-28 22:59:08 +0530

    Q1 Completed

---
+++ q1.py
@@ -0,0 +1,30 @@
+N = int(input())
+nums = list(map(int, input().split()))
+
+# Largest
+largest = nums[0]
+smallest = nums[0]
+total = 0
+even = 0
+odd = 0
+
+for i in nums:
+    if i>largest:
+        largest = i
+    if i<smallest:
+        smallest=i
+    total += i
+    if i%2 == 0:
+        even += 1
+    else:
+        odd += 1
+
+print("Largest: ", largest)
+print("Smallest: ", smallest)
+print("Sum: ", total)
+print("Even Count: ", even)
+print("Odd Count: ", odd)
+print("Reversed: ", end = " ")
+for i in range(len(nums)-1, -1, -1):
+    print(nums[i], end = " ")
+
